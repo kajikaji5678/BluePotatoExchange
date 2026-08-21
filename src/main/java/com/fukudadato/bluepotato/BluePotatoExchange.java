0ab560fc0d226f9c98ac387d41009c52ee2644ac
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-08-21 20:22:12 +0900

    setup-plugin

---
+++ src/main/java/com/fukudadato/bluepotato/BluePotatoExchange.java
@@ -0,0 +1,17 @@
+package com.fukudadato.bluepotato;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class BluePotatoExchange extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("BluePotatoExchange has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("BluePotatoExchange has been disabled!");
+    }
+}
