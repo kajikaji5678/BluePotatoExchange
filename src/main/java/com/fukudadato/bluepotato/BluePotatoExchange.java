d5a65a4bcfa0e4ea39eb160b45a5c3cae7d48dba
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-09-15 21:40:11 +0900

    修正

--- src/main/java/com/fukudadato/bluepotato/BluePotatoExchange.java
+++ src/main/java/com/fukudadato/bluepotato/BluePotatoExchange.java
@@ -8,6 +8,7 @@ public class BluePotatoExchange extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("BluePotatoExchange has been enabled!");
+        getLogger().info("hello!! KajiPlugin is Open!!");
     }
 
     @Override
